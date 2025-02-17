90e63f1bf6378ac1242c3320fa874f6f13de9fcf
ANKUR MISHRA <ankur25122@gmail.com>
2025-02-17 13:09:04 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Ensures it binds to all network interfaces
-    port: process.env.PORT || 5173, // Uses Render's assigned port or defaults to 5173
+    host: '0.0.0.0',  
+    port: process.env.PORT || 5173,  
+    strictPort: true, 
+    allowedHosts: ['personal-portfolio-padp.onrender.com'], 
+    cors: true
   }
 })
-
