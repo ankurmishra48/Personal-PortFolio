5a8258f89f4dbc15e6caa2ece87224ff2a767563
ANKUR MISHRA <ankur25122@gmail.com>
2025-02-17 13:16:10 +0530

    Initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
           <div className="md:w-1/2 mt-24 md:mt-36 space-y-2 order-2 md:order-1">
             <span className='text-xl'>Welcome In My Feed</span>
             <div className='flex space-x-1 text-2xl md:text-4xl'>
-              <h1>Hello, I'm Ankur a</h1>
+              <h1>Hello, I'm a</h1>
               <ReactTyped
                 className='text-red-700 font-bold'
                 strings={["Developer", "Programmer", "Coder"]}
