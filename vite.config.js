01c5f32fdd0cc80428b8896f859357a6d0d6841b
ANKUR MISHRA <ankur25122@gmail.com>
2025-02-17 13:05:25 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Ensures it binds to all network interfaces
+    port: process.env.PORT || 5173, // Uses Render's assigned port or defaults to 5173
+  }
 })
+
